4ceca876fb79129a5cbece63cdd345fea173db79
plOnion <damian.lukasiewicz@gmail.com>
2019-04-13 19:44:52 +0200

    add cover placeholder & clear list functionality & media queries

--- app.js
+++ app.js
@@ -1,40 +1,52 @@
 $(function () {
 
     const main = $(".js-content");
+    const booksList = document.querySelector(".js-content");
     let index = 0;
     let title = "";
     const input = document.querySelector(".filter__input");
-
-    // items[""0""].volumeInfo.title
-    // items[""0""].volumeInfo.imageLinks.thumbnail
-    // items[""0""].volumeInfo.description
+    let url = "";
 
     const showBooks = (books) => {
         books.items.forEach(book => {
+            let desc = "";
+            let placeholderImage = "book_placeholder.png";
+            let pict = "";
 
-            if (book.volumeInfo.description == undefined) {
-                book.volumeInfo.description = "Brak opisu" }
+            if (book.searchInfo == undefined) {
+                desc = "Brak opisu";
+            } else {
+                desc = book.searchInfo.textSnippet;
+            }
+
+            if (book.volumeInfo.imageLinks == undefined){
+                pict = placeholderImage;
+            } else {
+                pict = book.volumeInfo.imageLinks.thumbnail;
+            }
                 
             main.append(`<div class='book'>
-                    <div class="book__cover"><img src= ${book.volumeInfo.imageLinks.thumbnail} /></div>
-                    <div style="width: 80%">
+                    <div class="book__cover"><img src= ${pict} /></div>
+                    <div>
                         <h2 class='book__title'> ${book.volumeInfo.title} </h2>
                         <div class='book__description'>
-                            ${book.volumeInfo.description}
+                            ${desc}
                         </div>
                     </div>
                     </div>`);
         });
     };
 
-const showMore = () =>{
-    window.onscroll = function(ev) {
-        if ((window.innerHeight + Math.ceil(window.pageYOffset)) >= document.body.offsetHeight){
-        index = index + 10;
-        let url = "https://www.googleapis.com/books/v1/volumes?q="+title+"&startIndex="+index;
-        show(url);
+    const showMore = () =>{
+        window.onscroll = function(ev) {
+            if ((window.innerHeight + Math.ceil(window.pageYOffset)) >= document.body.offsetHeight){
+            index = index + 10;
+            url = "https://www.googleapis.com/books/v1/volumes?q="+title+"&startIndex="+index;
+            show(url);
+            }
+        }
     }
-}}
+
     const show = (url)=>{
         $.ajax({
             method: "GET",
@@ -47,8 +59,12 @@ const showMore = () =>{
     };
 
     input.addEventListener("change", ()=>{
+        if (booksList.hasChildNodes()) {
+            booksList.innerHTML = "";
+          }
         title = input.value;
-        let url = "https://www.googleapis.com/books/v1/volumes?q="+title+"&startIndex="+index;
+        url = "https://www.googleapis.com/books/v1/volumes?q="+title+"&startIndex="+index;
+        
         show(url);
         showMore();
     })
